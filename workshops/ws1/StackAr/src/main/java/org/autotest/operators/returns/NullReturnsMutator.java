f4de4314b43ab034e42e5babb2eeb52d66bf0917
GonzaloMBustos <bustosgonzalom@gmail.com>
2025-03-23 11:02:31 -0300

    more mutators

--- workshops/ws1/StackAr/src/main/java/org/autotest/operators/returns/NullReturnsMutator.java
+++ workshops/ws1/StackAr/src/main/java/org/autotest/operators/returns/NullReturnsMutator.java
@@ -4,6 +4,7 @@ import org.autotest.operators.MutationOperator;
 import spoon.reflect.code.CtExpression;
 import spoon.reflect.code.CtReturn;
 import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtTypeInformation;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,17 +18,27 @@ public class NullReturnsMutator extends MutationOperator {
     @Override
     public boolean isToBeProcessed(CtElement candidate) {
         // COMPLETAR
-        return false;
+        if (!(candidate instanceof CtReturn)) {
+            return false;
+        }
+
+        CtReturn op = (CtReturn)candidate;
+        return !op.getReturnedExpression().getType().isPrimitive();
     }
 
     @Override
     public void process(CtElement candidate) {
         // COMPLETAR
+        CtReturn op = (CtReturn)candidate;
+        op.setReturnedExpression(op.getFactory().Code().createLiteral(null));
     }
 
     @Override
     public String describeMutation(CtElement candidate) {
         // COMPLETAR
-        return null;
+        CtReturn op = (CtReturn)candidate;
+        return this.getClass().getSimpleName() + ": Se reemplazó " +
+                op.getReturnedExpression().toString() + " por null " +
+                " en la línea " + op.getPosition().getLine() + ".";
     }
 }
--- workshops/ws1/StackAr/src/main/java/org/autotest/operators/unary/IncrementsMutator.java
+++ workshops/ws1/StackAr/src/main/java/org/autotest/operators/unary/IncrementsMutator.java
@@ -1,7 +1,10 @@
 package org.autotest.operators.unary;
 
+import org.autotest.helpers.BinaryOperatorKindToString;
 import org.autotest.helpers.UnaryOperatorKindToString;
 import org.autotest.operators.MutationOperator;
+import spoon.reflect.code.BinaryOperatorKind;
+import spoon.reflect.code.CtBinaryOperator;
 import spoon.reflect.code.CtUnaryOperator;
 import spoon.reflect.code.UnaryOperatorKind;
 import spoon.reflect.declaration.CtElement;
@@ -18,17 +21,47 @@ public class IncrementsMutator extends MutationOperator {
     @Override
     public boolean isToBeProcessed(CtElement candidate) {
         // COMPLETAR
-        return false;
+        if (!(candidate instanceof CtUnaryOperator)) {
+            return false;
+        }
+
+        CtUnaryOperator op = (CtUnaryOperator)candidate;
+        List<UnaryOperatorKind> targetOperations = Arrays.asList(
+                UnaryOperatorKind.PREINC, // ++a
+                UnaryOperatorKind.PREDEC, // --a
+                UnaryOperatorKind.POSTINC, // a++
+                UnaryOperatorKind.POSTDEC // a--
+        );
+        return targetOperations.contains(op.getKind());
     }
 
     @Override
     public void process(CtElement candidate) {
         // COMPLETAR
+        CtUnaryOperator op = (CtUnaryOperator)candidate;
+        op.setKind(getReplacement(op.getKind()));
+    }
+
+    public UnaryOperatorKind getReplacement(UnaryOperatorKind kind) {
+        switch (kind) {
+            case PREINC:
+                return UnaryOperatorKind.PREDEC;
+            case PREDEC:
+                return UnaryOperatorKind.PREINC;
+            case POSTINC:
+                return UnaryOperatorKind.POSTDEC;
+            case POSTDEC:
+                return UnaryOperatorKind.POSTINC;
+        }
+        return null;
     }
 
     @Override
     public String describeMutation(CtElement candidate) {
         // COMPLETAR
-        return null;
+        CtUnaryOperator op = (CtUnaryOperator)candidate;
+        return this.getClass().getSimpleName() + ": Se reemplazó " +
+                UnaryOperatorKindToString.get(op.getKind()) + " por " + UnaryOperatorKindToString.get(getReplacement(op.getKind())) +
+                " en la línea " + op.getPosition().getLine() + ".";
     }
 }
